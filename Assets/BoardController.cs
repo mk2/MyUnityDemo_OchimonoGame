120f257593e7cbe3b4df90917917a6258b2507d4
mk2 <mk2@mk2.red>
2017-09-30 13:25:56 +0900

    ・連鎖判定のロジックを追加

--- Assets/BoardController.cs
+++ Assets/BoardController.cs
@@ -29,7 +29,9 @@ public class BoardController : MonoBehaviour
 
     const int BoardColumns = 6;
 
-    const int BoardRows = 12;
+    const int BoardRows = 6;
+
+    const int BoardMaxRows = 12;
 
     bool dragStart = false;
 
@@ -63,13 +65,12 @@ public class BoardController : MonoBehaviour
         {
             for (int column = 0; column < BoardColumns; column++)
             {
-                if (row < 7)
-                {
-                    var placeholder = Instantiate(BlockPlaceholerPrefab);
-                    LocateBlock(placeholder, row, column);
-                }
+                var placeholder = Instantiate(BlockPlaceholerPrefab);
+                placeholder.GetComponent<SpriteRenderer>().sortingOrder = -1;
+                LocateBlock(placeholder, row, column);
 
                 var block = NewBlock(board[row, column]);
+                block.GetComponent<SpriteRenderer>().sortingOrder = 1;
                 LocateBlock(block, row, column);
             }
         }
@@ -91,13 +92,13 @@ public class BoardController : MonoBehaviour
 
     void ResetBoard()
     {
-        board = new BlockType[BoardRows, BoardColumns];
-        boardObjects = new GameObject[BoardRows, BoardColumns];
+        board = new BlockType[BoardMaxRows, BoardColumns];
+        boardObjects = new GameObject[BoardMaxRows, BoardColumns];
         for (int row = 0; row < BoardRows; row++)
         {
             for (int col = 0; col < BoardColumns; col++)
             {
-                board[row, col] = (BlockType) Mathf.RoundToInt(Random.Range(1, 5));
+                board[row, col] = (BlockType)Random.Range(1, 5);
             }
         }
     }
@@ -113,6 +114,13 @@ public class BoardController : MonoBehaviour
 
         if (Input.GetMouseButtonUp(0))
         {
+            if (dragCount == MaxDragCount)
+            {
+                DeleteBoard();
+                FillBoard();
+                UpdateBoard();
+            }
+
             dragStart = false;
             dragCount = -1;
         }
@@ -124,37 +132,34 @@ public class BoardController : MonoBehaviour
             var pos = Camera.main.ScreenToWorldPoint(new Vector3(sx, sy, 0f));
             var px = pos.x;
             var py = pos.y;
-            var col = Mathf.RoundToInt((px - originX - blockSize / 2f) / blockSize);
-            var row = Mathf.RoundToInt((py - originY - blockSize / 2f) / blockSize);
+            var col = Mathf.FloorToInt((px - originX) / blockSize);
+            var row = Mathf.FloorToInt((py - originY) / blockSize);
 
-            if (dragCount < MaxDragCount && 
+            if (dragCount < MaxDragCount &&
                 (dragCount < 0 ||
                 !(mouseDownPosArray[dragCount, 0] == col && mouseDownPosArray[dragCount, 1] == row)))
             {
                 dragCount++;
                 mouseDownPosArray[dragCount, 0] = col;
                 mouseDownPosArray[dragCount, 1] = row;
-                Debug.Log(mouseDownPosArray[dragCount, 0] + "," + mouseDownPosArray[dragCount, 1]);
-            }
-        }
-
-        if (dragCount > 0)
-        {
-            for (int i = 0; i < dragCount; i++)
-            {
-                var px = mouseDownPosArray[i, 0];
-                var py = mouseDownPosArray[i, 1];
-                var go = boardObjects[py, px];
+                var go = boardObjects[row, col];
                 var color = go.GetComponent<SpriteRenderer>().color;
                 color.a = 0.5f;
                 go.GetComponent<SpriteRenderer>().color = color;
+                Debug.Log(mouseDownPosArray[dragCount, 0] + "," + mouseDownPosArray[dragCount, 1]);
             }
         }
     }
 
+    void NewRandomBlock(out BlockType blockType, out GameObject block)
+    {
+        blockType = (BlockType)Random.Range(1, 5);
+        block = NewBlock(blockType);
+    }
+
     GameObject NewBlock(BlockType blockType)
     {
-        switch(blockType)
+        switch (blockType)
         {
             case BlockType.Cherry:
                 return Instantiate(BlockCherryPrefab);
@@ -169,6 +174,322 @@ public class BoardController : MonoBehaviour
             default:
                 return null;
         }
+    }
+
+    void DeleteBoard()
+    {
+        for (var i = 0; i < MaxDragCount; i++)
+        {
+            var px = mouseDownPosArray[i, 0];
+            var py = mouseDownPosArray[i, 1];
+            Destroy(boardObjects[py, px]);
+            board[py, px] = BlockType.Empty;
+        }
+    }
+
+    /// <summary>
+    /// 消えた盤面を埋める
+    /// </summary>
+    void FillBoard()
+    {
+        // 列ごとにEmptyの数を調べ、上に並べる
+        for (int col = 0; col < BoardColumns; col++)
+        {
+            int emptyCount = 0;
+            for (int row = 0; row < BoardRows; row++)
+            {
+                if (board[row, col] == BlockType.Empty)
+                {
+                    // emptyCount分の新しいブロックを、BoardRowsより上に積み重ねる
+                    BlockType blockType;
+                    GameObject block;
+                    NewRandomBlock(out blockType, out block);
+                    block.GetComponent<SpriteRenderer>().sortingOrder = 1;
+                    int newBlockRow = BoardRows + emptyCount;
+                    board[newBlockRow, col] = blockType;
+                    LocateBlock(block, newBlockRow, col);
+                    emptyCount++;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 盤面で消せそうなものを消す
+    /// </summary>
+    bool UpdateBoard()
+    {
+        bool isUpdated = false;
+
+        var chainBlockCandidateDict = new Dictionary<int, Block>();
+
+        // 右下から1行ずつ連鎖チェックしていく(連鎖ブロック候補はマーキングすること)
+        for (int row = 0; row < BoardRows; row++)
+        {
+            for (int col = 0; col < BoardColumns; col++)
+            {
+                int idx = row * BoardColumns + col;
+                // 既に連鎖ブロック候補としてマーキングされていたらスキップする
+                if (chainBlockCandidateDict.ContainsKey(idx))
+                {
+                    continue;
+                }
+
+                // 上下左右をチェックし、同タイプのブロックがあったら連鎖候補としてマーキング
+                var checkList = new int[,]
+                {
+                    { 0, 1 }, // 上
+                    { 1, 0 }, // 右
+                    { 0, -1 },// 下
+                    { -1, 0 } // 左
+                };
+                for (int i = 0; i < 4; i++)
+                {
+                    BlockType myBlockType = board[row, col];
+                    if (myBlockType == BlockType.Empty)
+                    {
+                        continue;
+                    }
+
+                    int upX = col + checkList[i, 0];
+                    int upY = row + checkList[i, 1];
+                    if (0 <= upX && upX < BoardColumns && 0 <= upY && upY < BoardColumns)
+                    {
+                        BlockType yourBlockType = board[upY, upX];
+                        if (myBlockType == yourBlockType)
+                        {
+                            chainBlockCandidateDict.Add(idx, new Block { BlockType = myBlockType, Row = row, Col = col });
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        // 連鎖候補ブロックから、連鎖を抽出する
+        // とりあえずBlockTypeごとに検出する
+        var chainGroup = new ChainGroup();
+        foreach (var entry in chainBlockCandidateDict)
+        {
+            var block = entry.Value;
+            chainGroup.AddToChain(block);
+            Debug.Log("candidate=" + block);
+        }
+
+        foreach (var chain in chainGroup.UnionChains())
+        {
+            Debug.Log("NEW CHAIN: " + chain.Count);
+            if (chain.Count < 4)
+            {
+                continue;
+            }
+
+            foreach (var block in chain.Blocks)
+            {
+                var px = block.Col;
+                var py = block.Row;
+                var color = boardObjects[py, px].GetComponent<SpriteRenderer>().color;
+                color.r = 0;
+                boardObjects[py, px].GetComponent<SpriteRenderer>().color = color;
+            }
+        }
+
+        return isUpdated;
+    }
+}
+
+class ChainGroup
+{
+    public List<Chain> chainList = new List<Chain>();
+
+    public List<Chain> Chains
+    {
+        get { return chainList; }
+    }
+
+    public void AddToChain(Block block)
+    {
+        // 既存の連鎖に追加できないかチェックし、追加できなければ新しく連鎖を作る
+        bool isAdded = false;
+        foreach (var chain in chainList)
+        {
+            if (chain.BlockType != block.BlockType)
+            {
+                continue;
+            }
+
+            isAdded |= chain.AddBlockIfChained(block);
+        }
+
+        if (!isAdded)
+        {
+            var chain = new Chain
+            {
+                BlockType = block.BlockType
+            };
+            chain.AddBlock(block);
+            chainList.Add(chain);
+        }
+    }
+
+    public List<Chain> UnionChains()
+    {
+        var unionChains = new List<Chain>();
+        foreach (var myChain in chainList)
+        {
+            foreach (var otherChain in chainList)
+            {
+                if (myChain.IsOverlapped(otherChain))
+                {
+                    var chain = myChain.Union(otherChain);
+                    unionChains.Add(chain);
+                }
+            }
+        }
+        return unionChains;
+    }
+
+    public List<Chain> ValidChains()
+    {
+        var validChains = new List<Chain>();
+        foreach (var chain in chainList)
+        {
+            if (chain.Count > 3)
+            {
+                validChains.Add(chain);
+            }
+        }
+        return validChains;
+    }
+}
+
+class Chain
+{
+    public BlockType BlockType { get; set; }
+
+    List<Block> blocks = new List<Block>();
+
+    public List<Block> Blocks
+    {
+        get { return blocks; }
+    }
+
+    public int Count
+    {
+        get { return blocks.Count; }
+    }
+
+    public void Clear()
+    {
+        blocks.Clear();
+    }
+
+    public void AddBlock(Block block)
+    {
+        blocks.Add(block);
+    }
+
+    public bool AddBlockIfChained(Block block)
+    {
+        bool isChained = IsChained(block);
+        if (isChained)
+        {
+            blocks.Add(block);
+        }
+        return isChained;
+    }
+
+    public Chain Union(Chain otherChain)
+    {
+        var chain = new Chain();
+        chain.blocks.AddRange(blocks);
+        foreach (var otherBlock in otherChain.blocks)
+        {
+            bool isContained = false;
+            foreach (var myBlock in chain.blocks)
+            {
+                if (myBlock.Equals(otherBlock))
+                {
+                    isContained = true;
+                    break;
+                }
+            }
+            if (!isContained)
+            {
+                chain.blocks.Add(otherBlock);
+            }
+        }
+        return chain;
+    }
+
+    public bool IsOverlapped(Chain other)
+    {
+        bool isOverlapped = false;
+        foreach(var myBlock in blocks)
+        {
+            foreach (var otherBlock in other.blocks)
+            {
+                isOverlapped = myBlock.Equals(otherBlock);
+                if (isOverlapped)
+                {
+                    goto FinishOverlapCheck;
+                }
+            }
+        }
+        FinishOverlapCheck:
+        return isOverlapped;
+    }
+
+    public bool IsChained(Block other)
+    {
+        bool isChained = false;
+
+        foreach (var block in blocks)
+        {
+            isChained = block.IsClosed(other);
+            if (isChained)
+            {
+                break;
+            }
+        }
+
+        return isChained;
+    }
+
+}
+
+struct Block
+{
+    public BlockType BlockType { get; set; }
+
+    public int Row { get; set; }
+
+    public int Col { get; set; }
+
+    public bool IsClosed(Block other)
+    {
+        int distance = Mathf.Abs(this.Row - other.Row) + Mathf.Abs(this.Col - other.Col);
+        return distance == 1;
+    }
+
+    public override string ToString()
+    {
+        return "type=" + BlockType + " Row=" + Row + " Col=" + Col;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (!(obj is Block))
+        {
+            return false;
+        }
+        Block other = (Block)obj;
+        return Row == other.Row && Col == other.Col;
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
     }
 }
 
